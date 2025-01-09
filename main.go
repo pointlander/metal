7929d5c9ac74599d4d6401262c920006e9f488b5
Andrew Snodgrass <pointlander@gmail.com>
2025-01-09 15:27:35 -0600

    Added matrix math

--- main.go
+++ main.go
@@ -7,8 +7,164 @@ package main
 import (
 	"compress/bzip2"
 	"embed"
+	"fmt"
 	"io"
+	"math"
 )
+
+const (
+	// S is the scaling factor for the softmax
+	S = 1.0 - 1e-300
+)
+
+// Matrix is a float64 matrix
+type Matrix struct {
+	Cols int
+	Rows int
+	Data []float64
+}
+
+// NewMatrix creates a new float32 matrix
+func NewMatrix(cols, rows int, data ...float64) Matrix {
+	if data == nil {
+		data = make([]float64, 0, cols*rows)
+	}
+	return Matrix{
+		Cols: cols,
+		Rows: rows,
+		Data: data,
+	}
+}
+
+// MulT multiplies two matrices and computes the transpose
+func (m Matrix) MulT(n Matrix) Matrix {
+	if m.Cols != n.Cols {
+		panic(fmt.Errorf("%d != %d", m.Cols, n.Cols))
+	}
+	columns := m.Cols
+	o := Matrix{
+		Cols: m.Rows,
+		Rows: n.Rows,
+		Data: make([]float64, 0, m.Rows*n.Rows),
+	}
+	lenn, lenm := len(n.Data), len(m.Data)
+	for i := 0; i < lenn; i += columns {
+		nn := n.Data[i : i+columns]
+		for j := 0; j < lenm; j += columns {
+			mm := m.Data[j : j+columns]
+			o.Data = append(o.Data, dot(mm, nn))
+		}
+	}
+	return o
+}
+
+// Add adds two float32 matrices
+func (m Matrix) Add(n Matrix) Matrix {
+	lena, lenb := len(m.Data), len(n.Data)
+	if lena%lenb != 0 {
+		panic(fmt.Errorf("%d %% %d != 0", lena, lenb))
+	}
+
+	o := Matrix{
+		Cols: m.Cols,
+		Rows: m.Rows,
+		Data: make([]float64, 0, m.Cols*m.Rows),
+	}
+	for i, value := range m.Data {
+		o.Data = append(o.Data, value+n.Data[i%lenb])
+	}
+	return o
+}
+
+func (m Matrix) Softmax(T float64) Matrix {
+	output := NewMatrix(m.Cols, m.Rows)
+	max := 0.0
+	for _, v := range m.Data {
+		v /= T
+		if v > max {
+			max = v
+		}
+	}
+	s := max * S
+	for i := 0; i < len(m.Data); i += m.Cols {
+		sum := 0.0
+		values := make([]float64, m.Cols)
+		for j, value := range m.Data[i : i+m.Cols] {
+			values[j] = math.Exp(value/T - s)
+			sum += values[j]
+		}
+		for _, value := range values {
+			output.Data = append(output.Data, value/sum)
+		}
+	}
+	return output
+}
+
+// T tramsposes a matrix
+func (m Matrix) T() Matrix {
+	o := Matrix{
+		Cols: m.Rows,
+		Rows: m.Cols,
+		Data: make([]float64, 0, m.Cols*m.Rows),
+	}
+	for i := 0; i < m.Cols; i++ {
+		for j := 0; j < m.Rows; j++ {
+			o.Data = append(o.Data, m.Data[j*m.Cols+i])
+		}
+	}
+	return o
+}
+
+func dot(x, y []float64) (z float64) {
+	for i := range x {
+		z += x[i] * y[i]
+	}
+	return z
+}
+
+func softmax(values []float64) {
+	max := 0.0
+	for _, v := range values {
+		if v > max {
+			max = v
+		}
+	}
+	s := max * S
+	sum := 0.0
+	for j, value := range values {
+		values[j] = math.Exp(value - s)
+		sum += values[j]
+	}
+	for j, value := range values {
+		values[j] = value / sum
+	}
+}
+
+// SelfAttention computes the self attention of Q, K, V
+func SelfAttention(Q, K, V Matrix) Matrix {
+	o := Matrix{
+		Cols: V.Cols,
+		Rows: K.Rows,
+		Data: make([]float64, 0, V.Rows*K.Rows),
+	}
+	outputs, values := make([]float64, V.Cols), make([]float64, Q.Rows)
+	V = V.T()
+	for i := 0; i < K.Rows; i++ {
+		K := K.Data[i*K.Cols : (i+1)*K.Cols]
+		for j := 0; j < Q.Rows; j++ {
+			Q := Q.Data[j*Q.Cols : (j+1)*Q.Cols]
+			values[j] = dot(K, Q)
+		}
+		softmax(values)
+
+		for j := 0; j < V.Rows; j++ {
+			V := V.Data[j*V.Cols : (j+1)*V.Cols]
+			outputs[j] = dot(values, V)
+		}
+		o.Data = append(o.Data, outputs...)
+	}
+	return o
+}
 
 //go:embed 84.txt.utf-8.bz2
 var Data embed.FS
