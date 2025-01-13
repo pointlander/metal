0ed83be08b06c2ccef9248fac87d37914eaa4f6a
Andrew Snodgrass <pointlander@gmail.com>
2025-01-13 17:49:17 -0600

    Fast but inaccurate mode mach3

--- main.go
+++ main.go
@@ -304,6 +304,8 @@ var (
 	FlagMach1 = flag.Bool("mach1", false, "mach 1 version")
 	// FlagMach2 is the mach 2 version
 	FlagMach2 = flag.Bool("mach2", false, "mach 2 version")
+	// FlagMach3 is the mach 3 version
+	FlagMach3 = flag.Bool("mach3", false, "mach 3 version")
 	// FlagQuery is the query string
 	FlagQuery = flag.String("query", "What is the meaning of life?", "query flag")
 	// FlagBuild build the database
@@ -613,6 +615,72 @@ func Mach2() {
 	fmt.Println(string(result))
 }
 
+// Mach3 is the mach 3 mode
+func Mach3() {
+	file, err := Data.Open("84.txt.utf-8.bz2")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	reader := bzip2.NewReader(file)
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		panic(err)
+	}
+
+	model := [64 * 1024][512]float32{}
+	fmt.Println(64 * 1024 * 512 * 4.0 / (1024.0 * 1024.0 * 1024.0))
+	context := uint(0)
+	m := NewMixer()
+	for _, v := range data {
+		vector := m.Mix().Sum()
+		context = ((context << 8) | (uint(v) & 0xFF)) & 0xFFFF
+		for i := range model[context][:256] {
+			model[context][i] += float32(vector.Data[i])
+		}
+		model[context][256+uint(v)]++
+		m.Add(v)
+	}
+
+	rng := rand.New(rand.NewSource(1))
+
+	m = NewMixer()
+	for _, v := range []byte(*FlagQuery) {
+		m.Add(v)
+	}
+
+	result := make([]byte, 0, 8)
+	for i := 0; i < 33; i++ {
+		vector := m.Mix()
+		distro := vector.Sum()
+		index, max := 0, float32(0.0)
+		for i := range model {
+			cs := CS(model[i][:256], distro.Data)
+			if cs > max {
+				max, index = cs, i
+			}
+		}
+		x := NewMatrix(len(model[index]), 1)
+		for _, v := range model[index] {
+			x.Data = append(x.Data, float64(v))
+		}
+		x = x.Softmax(1)
+		sum, selected, symbol := float32(0.0), rng.Float32(), 0
+		for i, v := range x.Data {
+			sum += float32(v)
+			if selected < sum {
+				symbol = i
+				break
+			}
+		}
+
+		fmt.Printf("%d %s\n", symbol, strconv.Quote(string(byte(symbol))))
+		m.Add(byte(symbol))
+		result = append(result, byte(symbol))
+	}
+	fmt.Println(string(result))
+}
+
 func main() {
 	flag.Parse()
 
@@ -621,6 +689,9 @@ func main() {
 		return
 	} else if *FlagMach2 {
 		Mach2()
+		return
+	} else if *FlagMach3 {
+		Mach3()
 		return
 	}
 }
